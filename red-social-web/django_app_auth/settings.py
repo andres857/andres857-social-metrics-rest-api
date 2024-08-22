fa55c20b95dcc52881087a9985c8d8ba37fc486b
Andres-Alfonso <falfonso@windowschannel.com>
2024-08-22 12:11:19 -0500

    se agrega test de email

--- red-social-web/django_app_auth/settings.py
+++ red-social-web/django_app_auth/settings.py
@@ -213,6 +213,17 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Envio de correo electronico para desarrollo
+#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
+#EMAIL_USE_TLS = True
+#EMAIL_HOST_USER = ''
+#EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT=os.environ.get('EMAIL_PORT')
 
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
