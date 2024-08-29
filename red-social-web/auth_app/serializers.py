f254d609f6fb764fcb6b675acacea64a700c40b1
Andres-Alfonso <falfonso@windowschannel.com>
2024-08-29 16:09:35 -0500

    Se crea logica de register

--- red-social-web/auth_app/serializers.py
+++ red-social-web/auth_app/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+from allauth.account.models import EmailAddress
 
 User = get_user_model()
 
@@ -10,11 +11,12 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'password', 'password2', 'first_name', 'last_name', 
+        fields = ('id', 'email', 'password', 'password2', 'first_name', 'last_name', 'identification',
                   'phone')
         extra_kwargs = {
             'first_name': {'required': True},
             'last_name': {'required': True},
+            'identification': {'required': True},
             'email': {'required': True}
         }
 
@@ -26,4 +28,13 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop('password2')
         user = User.objects.create_user(**validated_data)
+        
+        # Crear la entrada en account_emailaddress
+        EmailAddress.objects.create(
+            user=user,
+            email=user.email,
+            verified=False,
+            primary=True
+        )
+        
         return user
