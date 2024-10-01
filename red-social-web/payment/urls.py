d27a7f34c29dd3814b918cc8b7678442dcd370a7
andres-alfonso <falfonso@windowschannel.com>
2024-10-01 11:59:56 -0500

    se ajusta logica en pago

--- red-social-web/payment/urls.py
+++ red-social-web/payment/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     #path('orders', views.create_order_t, name='orders'),
     path('create-subscription/', views.create_preference, name='create_subscription'),
     path('success/', views.payment_success, name='payment_success'),
+    path('failure/', views.payment_failure, name='payment_failure'),
     path('api/mercadopago/webhook/', views.mercadopago_webhook, name='mercadopago_webhook'),
     path('get/pricing/', views.pricing, name='get_pricing'),
 ]
