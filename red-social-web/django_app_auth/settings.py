cabb6a1b672c490a70c81121e8d5d4170e07388e
andres-alfonso <falfonso@windowschannel.com>
2024-10-17 09:26:24 -0500

    Se configura smtp2go

--- red-social-web/django_app_auth/settings.py
+++ red-social-web/django_app_auth/settings.py
@@ -246,13 +246,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
-EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+# EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_PORT=os.environ.get('EMAIL_PORT')
+
 #EMAIL_USE_TLS = True
 #EMAIL_HOST_USER = ''
 #EMAIL_HOST_PASSWORD = ''
+
+# SMTP2TOGO
+EMAIL_HOST=os.environ.get('EMAIL_HOST')
 EMAIL_PORT=os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS=os.environ.get('EMAIL_USE_TLS')
+EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL=os.environ.get('DEFAULT_FROM_EMAIL')
 
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
